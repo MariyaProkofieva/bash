2e6cca519b40c3f1efae3d4040cb48e4c99c4cba
Kurahashi Archie <kurahaar@ga-i5.msk.21-school.ru>
2024-05-05 21:45:39 +0300

    22

---
+++ cat/s21_cat.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+
+const struct option longopts[] = {
+    {"number-nonblank", no_argument, NULL, 'b'},
+    {"number", no_argument, NULL, 'n'},
+    {"squeeze-blank", no_argument, NULL, 's'},
+    {0, 0, 0, 0}
+};
+
+typedef struct {
+    int b;
+    int e;
+    int E;
+    int v;
+    int n;
+    int s;
+    int t;
+    int T;
+} shortopts;
+
+void open_close(char *filename, shortopts* flags);
+
+
+void cat(shortopts* flags, FILE *f);
