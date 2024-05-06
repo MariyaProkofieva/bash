ed56d954c1c3af1620d92073c8b4d7aaaecc58ba
Kurahashi Archie <kurahaar@ga-h5.msk.21-school.ru>
2024-05-06 21:49:56 +0300

    grep

---
+++ cat/grep/s21_grep.c
@@ -0,0 +1,77 @@
+#include "s21_grep.h"
+
+void parser(flags *fl); 
+void process_file();
+void compile_reg(regex_t,)
+
+int main(int argc, char *argv[]) {
+
+    flags fl = {0};
+    char *pattern
+
+    return 0;
+
+}
+
+ parser(flags *fl, argc, argv){
+
+        int opt = 0;
+
+        while(opt!= -1){
+            opt = getopt(argc, argv, "e:f:ivclnhso") // : требует аргумента 
+            switch(flag){
+                case 'e':
+                fl->e = 1;
+                break;
+                case 'i':
+                fl->i = 1;
+                break;
+                case 'v':
+                fl->v = 1;
+                break;
+                case 'c':
+                fl->c = 1;
+                break;
+                case 'l':
+                fl->l = 1;
+                break;
+                case 'n':
+                fl->n = 1;
+                break;
+                case 'h':
+                fl->h = 1;
+                break;
+                case 's':
+                fl->s = 1;
+                break;
+                case 'f':
+                fl->f = 1;
+                break;
+                case 'o':
+                fl->o = 1;
+                break;
+
+            }
+        }
+    }
+
+
+
+
+void process_file(*char filename){
+    FILE *file = fopen(filename, "r");
+    if(file != NULL){
+        regex_t regex; 
+        compile_reg(&regex,);
+
+        fclose(file);
+    } else if (!fl.s){ /// этот флаг для подавления сообщения об ошибке 
+        printf("s21_grep: %s: No such file or directory\n", filename);
+    }
+}
+
+
+
+void reg_compile(regex_t *regex, pattern, flags fl){
+    regcomp(regex, pattern, fl.i)
+}
---
+++ cat/grep/s21_grep.h
@@ -0,0 +1,16 @@
+#include <getopt.h>
+#include <regex.h>
+
+
+typedef struct {
+    int i;
+    int e;
+    int c;
+    int v;
+    int n;
+    int l;
+    int h;
+    int s;
+    int f;
+    int o;
+} flags;
